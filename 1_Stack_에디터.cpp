94d6399b48a614b40cb4ec56b9efb4738d268444
scarlett <youjin9200@naver.com>
2018-09-09 13:58:22 +0900

    Update 1_Stack_에디터.cpp

--- 1_Stack_에디터.cpp
+++ 1_Stack_에디터.cpp
@@ -53,21 +53,28 @@ int main() {
     while (n-- > 0) {
         char cmd;
         scanf(" %c", &cmd);
+        // 1) 커서를 왼쪽으로 한 칸 옮김 (커서가 문장의 맨 앞이면 무시됨)
         if (cmd == 'L') {
             if (!left.empty()) {
                 right.push(left.top());
                 left.pop();
             }
-        } else if (cmd == 'D') {
+        } 
+        // 2) 커서를 오른쪽으로 한 칸 옮김 (커서가 문장의 맨 뒤이면 무시됨)
+        else if (cmd == 'D') {
             if (!right.empty()) {
                 left.push(right.top());
                 right.pop();
             }
-        } else if (cmd == 'B') {
+        } 
+        // 3) 커서 왼쪽에 있는 문자를 삭제함 (커서가 문장의 맨 앞이면 무시됨)
+        else if (cmd == 'B') {
             if (!left.empty()) {
                 left.pop();
             }
-        } else if (cmd == 'P') {
+        } 
+        // 4) $라는 문자를 커서 왼쪽에 추가함
+        else if (cmd == 'P') {
             char c;
             scanf(" %c", &c);
             left.push(c);
